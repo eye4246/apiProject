d2ac9072e3a7ac0f6335d293b98c8366b256530e
Eric Ye <ericye134@gmail.com>
2024-02-24 17:09:18 -0500

    Shiba renders

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("This is the query selector",button)
 button.addEventListener("click", myFunction)
 const apiData = {
     url: "http://shibe.online/api/shibes",
-    parameter: "?count=2",
+    parameter: "?count=1",
     q: "&",
     urls: "urls=true",
     httpsUrls: "httpsUrls=true",
@@ -11,9 +11,22 @@ const apiData = {
 //http://shibe.online/api/shibes?count=2&urls=true&httpsUrls=true
 const { url, parameter, q, urls , httpsUrls } = apiData;
 const apiURL = `${url}${parameter}${q}${urls}${q}${httpsUrls}`;
-console.log("This the api url", apiURL)
+//console.log("This the api url", apiURL)
+
+
 function myFunction(){
+    //document.getElementById(".myButton").innerHTML="<img src='http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true' alt='shiba' width='100%' />";
     console.log("I am in my function")
     fetch(apiURL)
-        .then((result) => console.log("",result.json()));
-    }
+        .then((result) => result.json())
+        .then((data) => {
+        data.forEach((shiba) => {
+            //console.log(Count)
+            //setAttribute("href", shiba.url)
+            let i = document.createElement("img");;
+            i.setAttribute("src", shiba);
+                i.setAttribute("width", "100%");
+                document.body.appendChild(i);
+    })
+})
+}
